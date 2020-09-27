355a9636ba7eff0ef0b7a17c59668d0b51ddb2a9
Mehru481 <70485759+Mehru481@users.noreply.github.com>
2020-09-28 02:12:59 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,51 @@
+//ByeGreeter.ByeGreet();
+//HelloGreeter.HelloGreet();
+
+(function (name) {
+  console.log(HelloGreeter.Greet + name);
+})
+("Yakoov");
+
+(function (name) {
+  console.log(ByeGreeter.Greet + name);
+})
+("John");
+(function (name) {
+  console.log(ByeGreeter.Greet + name);
+})
+("Jen");
+(function (name) {
+  console.log(ByeGreeter.Greet + name);
+})
+("Jason");
+
+
+(function (name) {
+  console.log(HelloGreeter.Greet + name);
+})
+("Pual");
+
+(function (name) {
+  console.log(HelloGreeter.Greet + name);
+})
+("Frank");
+(function (name) {
+  console.log(HelloGreeter.Greet + name);
+})
+("Larry");
+
+(function (name) {
+  console.log(HelloGreeter.Greet + name);
+})
+("Plank");
+
+(function (name) {
+  console.log(HelloGreeter.Greet + name);
+})
+("Laura");
+
+(function (name) {
+  console.log(ByeGreeter.Greet + name);
+})
+("Jim");
+
